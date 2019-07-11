4e17db53357d18007504809a6afa136d63f86b7b
bvidot <bvidot@ankama.com>
2019-07-11 23:06:19 +0200

    Add Move & Dash with XInput
    Ajout du déplacement à la manette et du Dash

--- GameJamAnkama/Assets/Scripts/PlayerController/KeyboardInputMgr.cs
+++ GameJamAnkama/Assets/Scripts/PlayerController/KeyboardInputMgr.cs
@@ -1,19 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XInputDotNetPure;
 
 public class KeyboardInputMgr : InputManager
 {
+    public PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        Debug.Log(direction);
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+
+        Vector3 direction = new Vector3(state.ThumbSticks.Left.X, 0, state.ThumbSticks.Left.Y);
         float angle = Vector3.SignedAngle(Vector3.forward, direction, this.transform.up);
         _playerController.Move(angle, !direction.Equals(Vector3.zero));
-        if (Input.GetButtonUp("Jump"))
+
+        if (state.Buttons.A == ButtonState.Pressed)
             _playerController.Eat();
-        if (Input.GetButtonUp("Submit"))
+        if (state.Buttons.RightShoulder == ButtonState.Pressed)
             _playerController.Dash();
     }
 }
--- GameJamAnkama/Assets/Scripts/PlayerController/PlayerController.cs
+++ GameJamAnkama/Assets/Scripts/PlayerController/PlayerController.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float _speed               = 1.0f;
-    [SerializeField] private float _dashIntensity       = 1.0f;
-    [SerializeField] private float _dashSpeed           = 3.0f;
-    [SerializeField] private float _speedBoostFactor    = 2.0f;
-    [SerializeField] private float _speedBoostDuration  = 2.0f;
+    [SerializeField] private float _speed                   = 1.0f;
+    [SerializeField] private float _dashDuration            = 0.2f;
+    [SerializeField] private float _dashSpeed               = 3.0f;
+    [SerializeField] private float _dashCost                = 15.0f;
+    [SerializeField] private float _dashJaugeMax            = 30f;
+    [SerializeField] private float _dashJaugeRecoverDelay   = 15f;
+    [SerializeField] private float _dashRecoverAmount       = 1.0f;
+    [SerializeField] private float _speedBoostFactor        = 2.0f;
+    [SerializeField] private float _speedBoostDuration      = 2.0f;
+    [SerializeField] private float _speedAppleCost          = 1.0f;
 
     private Animator    _animator;
     private bool        _isBoosted  = false;
     private bool        _dashing    = false;
 
     private Vector3     _wantedPosition;
-    private Vector3     _startPosition;
+
+    private float _currentDashJauge = 30f;
+  
 
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (_currentDashJauge < _dashJaugeMax)
+            DashRecover();
     }
 
     ///////////////////////////////////
@@ -31,8 +44,9 @@ public class PlayerController : MonoBehaviour
         if (!move)
             return;
         if (!_dashing)
-            this.transform.rotation = Quaternion.Euler(0, angle, 0);
-        this.transform.Translate(this.transform.forward * (_dashing ? _dashSpeed : _speed));
+            this.transform.rotation = Quaternion.Euler(0, angle-45, 0);
+
+        this.transform.position += transform.forward * Time.deltaTime * (_dashing ? _dashSpeed : _speed);
 
         //Set le bon angle d'animation
         //De face si idle
@@ -43,18 +57,29 @@ public class PlayerController : MonoBehaviour
     ///////////////////////////////////
     public void Eat()
     {
+        if (_isBoosted)
+            return;
+
         _isBoosted = true;
         SpeedBoost();
+        ConsumeApple();
     }
 
     private void SpeedBoost()
     {
         _speed *= _speedBoostFactor;
+        Invoke("UnSpeedBoost", _speedBoostDuration);
     }
 
     private void UnSpeedBoost()
     {
+        _isBoosted = false;
         _speed /= _speedBoostFactor;
+    }
+
+    private void ConsumeApple()
+    {
+        
     }
 
     ///////////////////////////////////
@@ -62,13 +87,21 @@ public class PlayerController : MonoBehaviour
     ///////////////////////////////////
     public void Dash()
     {
+        if (_dashing || _currentDashJauge < _dashCost)
+            return;
+
         _dashing = true;
-        _startPosition = this.transform.position;
-        Invoke("ResetDash", _dashIntensity); 
+        _currentDashJauge -= _dashCost;
+        Invoke("ResetDash", _dashDuration); 
     }
 
     private void ResetDash()
     {
         _dashing = false;
+    }
+
+    private void DashRecover()
+    {
+        _currentDashJauge = Mathf.Min(_currentDashJauge + _dashRecoverAmount * Time.deltaTime, _dashJaugeMax);
     }
 }
