1eec1585ab2c31e2020785b3bb0167bc18b23ba6
Nicolas Diers <ndiers@ankama.com>
2019-07-12 09:40:46 +0200

    Auto stash before merge of "master" and "origin/master"

--- GameJamAnkama/Assets/Scripts/TreeApplesGrow.cs
+++ GameJamAnkama/Assets/Scripts/TreeApplesGrow.cs
@@ -9,14 +9,14 @@ public class TreeApplesGrow : MonoBehaviour
     public float popVariance;
     public GameObject applePrefab;
 
-
-    private Queue<GameObject> _apples;
+    
     private bool _coroutineRunning;
+    private Dictionary<Vector3, GameObject> _apples;
 
     // Start is called before the first frame update
     void Start()
     {
-        _apples = new Queue<GameObject>();
+        _apples = new Dictionary<Vector3, GameObject>();
         StartCoroutine("GrowApples");
         _coroutineRunning = true;
     }
@@ -27,21 +27,43 @@ public class TreeApplesGrow : MonoBehaviour
         
     }
 
-    public void DropApple()
+    public void DropApples()
     {
-        GameObject droppedApple = _apples.Dequeue();
-        droppedApple.GetComponent<Rigidbody>().useGravity = true;
+        foreach (KeyValuePair<Vector3, GameObject> keyValuePair in _apples)
+        {
+            keyValuePair.Value.GetComponent<Rigidbody>().useGravity = true;
+            _apples.Remove(keyValuePair.Key);
+        }
+        gameObject.GetComponent<Rigidbody>().AddExplosionForce(5, transform.position, 18, 5);
         if (!_coroutineRunning)
             StartCoroutine("GrowApples");
     }
 
     private void GrowApple()
     {
-        GameObject apple = Instantiate(applePrefab, gameObject.transform, false);
-        apple.transform.Translate(new Vector3(0, 4, 0));
+        GameObject apple = Instantiate(applePrefab);
         apple.transform.parent = gameObject.transform;
+        apple.transform.localPosition = Vector3.zero;
+        Vector3 vectorTemp;
+        bool validAngle = true;
+        int tries = 0;
+        do
+        {
+            tries += 1;
+            vectorTemp = new Vector3(Random.Range(-0.5f, 0.5f), 7 + Random.Range(-4, 4), Random.Range(-0.5f, 0.5f));
+            foreach (KeyValuePair<Vector3, GameObject> keyValuePair in _apples)
+            {
+                if (Vector3.Angle(vectorTemp, keyValuePair.Key) < 20)
+                {
+                    validAngle = false;
+                    break;
+                }
+            }
+        } while (!validAngle && tries<10);
+        _apples[vectorTemp] = apple;
+        apple.transform.Translate(vectorTemp);
         apple.GetComponent<Rigidbody>().useGravity = false;
-        _apples.Enqueue(apple);
+        
         if (_apples.Count >= maxApples)
         {
             StopCoroutine("GrowApples");
