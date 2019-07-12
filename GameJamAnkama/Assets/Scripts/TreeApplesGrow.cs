8a5dc96828ef05957bafa443ecb4e3e0663e4baf
Nicolas Diers <ndiers@ankama.com>
2019-07-12 15:47:38 +0200

    prefab resize

--- GameJamAnkama/Assets/Scripts/TreeApplesGrow.cs
+++ GameJamAnkama/Assets/Scripts/TreeApplesGrow.cs
@@ -11,11 +11,13 @@ public class TreeApplesGrow : MonoBehaviour
 
     
     private bool _coroutineRunning;
+    private bool _fullTree;
     private Dictionary<Vector3, GameObject> _apples;
 
     // Start is called before the first frame update
     void Start()
     {
+        _fullTree = false;
         _apples = new Dictionary<Vector3, GameObject>();
         StartCoroutine("GrowApples");
         _coroutineRunning = true;
@@ -29,23 +31,29 @@ public class TreeApplesGrow : MonoBehaviour
 
     public void OnTriggerExit(Collider other)
     {
-        if(other.name.Contains("apple"))
+        if(_apples.ContainsValue(other.gameObject))
+        {
             other.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            other.GetComponent<Rigidbody>().isKinematic = true;
+        }
     }
 
     public void DropApples()
     {
-        Vector3 v;
         foreach (KeyValuePair<Vector3, GameObject> keyValuePair in _apples)
         {
-            keyValuePair.Value.GetComponent<Rigidbody>().useGravity = true;
-            v = keyValuePair.Key;
-            v.y = 8;
-            keyValuePair.Value.GetComponent<Rigidbody>().AddForce(Vector3.Normalize(v)*5, ForceMode.Impulse);
+            DropApple(keyValuePair.Key, keyValuePair.Value);
         }
         _apples.Clear();
-        if (!_coroutineRunning)
-            StartCoroutine("GrowApples");
+    }
+
+    private void DropApple(Vector3 key, GameObject value)
+    {
+        value.GetComponent<Rigidbody>().useGravity = true;
+        Vector3 v = key;
+        v.y = 4;
+        value.GetComponent<Rigidbody>().isKinematic = false;
+        value.GetComponent<Rigidbody>().AddForce(Vector3.Normalize(v) * 4, ForceMode.Impulse);
     }
 
     private void GrowApple()
@@ -57,7 +65,7 @@ public class TreeApplesGrow : MonoBehaviour
         {
             validAngle = true;
             tries += 1;
-            vectorTemp = new Vector3(Random.Range(-0.3f, 0.3f), 12.5f + Random.Range(-1f, 1f), Random.Range(-0.3f, 0.3f));
+            vectorTemp = new Vector3(Random.Range(-0.3f, 0.3f), 4f + Random.Range(-1f, 1f), Random.Range(-0.3f, 0.3f));
             Vector2 vecCalc1 = new Vector2();
             Vector2 vecCalc2 = new Vector2();
             foreach (KeyValuePair<Vector3, GameObject> keyValuePair in _apples)
@@ -80,35 +88,38 @@ public class TreeApplesGrow : MonoBehaviour
         } while (!validAngle && tries<100);
         if(validAngle)
         {
+            if (_apples.Count == maxApples)
+            {
+                Vector3[] keys = new Vector3[maxApples];
+                _apples.Keys.CopyTo(keys, 0);
+                DropApple(keys[maxApples-1], _apples[keys[maxApples-1]]);
+                _apples.Remove(keys[maxApples-1]);
+                _fullTree = true;
+            }
+            else
+                _fullTree = false;
             GameObject apple = Instantiate(applePrefab);
             apple.transform.parent = gameObject.transform;
             apple.transform.localPosition = Vector3.zero;
             apple.transform.Translate(vectorTemp);
             apple.GetComponent<Rigidbody>().useGravity = false;
             vectorTemp.y = 0;
-            Debug.DrawRay(apple.transform.position, Vector3.Normalize(vectorTemp) * 10, Color.red, 5);
             apple.GetComponent<Rigidbody>().AddForce(Vector3.Normalize(vectorTemp) * 10, ForceMode.Impulse);
             Vector3 v = apple.GetComponent<Rigidbody>().velocity;
             v.y = 0;
             apple.GetComponent<Rigidbody>().velocity = v;
             _apples[vectorTemp] = apple;
-
-            if (_apples.Count >= maxApples)
-            {
-                StopCoroutine("GrowApples");
-                _coroutineRunning = false;
-            }
         }
     }
 
     private IEnumerator GrowApples()
     {
         _coroutineRunning = true;
-        yield return new WaitForSeconds(averagePop + Random.Range(-1 * popVariance, popVariance));
+        yield return new WaitForSeconds(_fullTree? averagePop*2 : averagePop + Random.Range(-1 * popVariance, popVariance));
         while (true)
         {
             GrowApple();
-            yield return new WaitForSeconds(averagePop + Random.Range(-1 * popVariance, popVariance));
+            yield return new WaitForSeconds(_fullTree ? averagePop * 2 : averagePop + Random.Range(-1 * popVariance, popVariance));
         }
     }
 }
